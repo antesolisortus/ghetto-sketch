6174a43774212bffc0ca49a019e0415e35bd3409
antesolisortus <git@subobcens.us>
2016-01-16 20:56:30 -0800

    New gradient color mode..

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,8 @@ var myHeight = 800;
 var myWidth = 800;
 var pixelCount = 100;
 var pixelBorderWidth = 0;
+var newColorAtBoundary = false;
+var colorGradientWhileDrawing = true;
 
 var sketchPad = $("#container");
 sketchPad.height(myHeight);
@@ -20,21 +22,83 @@ for(var i = 0; i < (pixelCount * pixelCount); i++) {
 }
 
 var drawColor = "black";
+var red = Math.floor(Math.random() * 512) - 256;
+var green = Math.floor(Math.random() * 512) - 256;
+var blue = Math.floor(Math.random() * 512) - 256;
+
+function decToHex(n) {
+	return ( "00" + n.toString(16) ).substr(-2);
+}
 function randoColor() {
 	var colString = "#";
 	for (var i = 0; i < 3; i++) {
-		var randHex = Math.floor((Math.random() * 256)).toString(16);
-		colString = colString.concat(("00" + randHex).substr(-2));
+		var randHex = Math.floor((Math.random() * 256))
+		colString = colString.concat(decToHex(randHex));
 	}
 	return colString;
+}
+function RGBtoHex(r, g, b) {
+	var colString = "#";
+	colString = colString.concat(decToHex(r));
+	colString = colString.concat(decToHex(g));
+  colString = colString.concat(decToHex(b));
+  return colString;
+}
+function modifyComponent(c, step) {
+	var dir = 0;
+	var col = Math.abs(c);
+	if (c >= 0) {
+		dir = 1;
+		var newC = col + (step * dir);
+		if (newC > 255) {
+			newC = -255;
+		}
+		return newC;
+	}
+	else {
+		dir = -1;
+		var newC = col + (step * dir);
+		if (newC <= 0) {
+			newC = 1;
+			return newC;
+		}
+		else {
+			return newC * dir; 
+		}
+	}
+
+}
+function stepColor(r, g, b) {
+	var chance = Math.random();
+	if (chance < (1/3)) {
+		var stepC = modifyComponent(r, 5);
+		red = stepC;
+		return RGBtoHex(Math.abs(red), g, b);
+	} 
+	else if (chance < (2/3)) {
+		var stepC = modifyComponent(g, 3);
+		green = stepC;
+		return RGBtoHex(r, Math.abs(green), b);
+	}
+	else {
+		var stepC = modifyComponent(b, 2);
+		blue = stepC;
+		return RGBtoHex(r, g, Math.abs(b));
+	}
 }
 
 $("div.pixel").mouseover(function() {
 	$(this).css("background-color", drawColor);
+	if (colorGradientWhileDrawing) {
+		drawColor = stepColor(red, green, blue);
+		console.log(red, green, blue);
+	}
 });
 
-$("#container").mouseleave(function() {
-	drawColor = randoColor();
-	console.log(drawColor);
-})
+if (newColorAtBoundary) {
+	$("#container").mouseleave(function() {
+		drawColor = randoColor();
+		console.log(drawColor);
+	});
+}
 
